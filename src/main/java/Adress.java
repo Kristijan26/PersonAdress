863cbc8133c4fd6fc1d1021a5b2c81449ba0fb07
Kritijan <kbojlevski41@gmail.com>
2024-04-01 20:07:22 +0200

    Personal Adress

---
+++ src/main/java/Adress.java
@@ -0,0 +1,22 @@
+public class Adress {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person obj_1 = new Person();
+		obj_1.setName("Filip");
+		obj_1.setAdress("St. 22 /95A");
+		obj_1.setPobox(1000);
+		
+		
+		
+		System.out.println("The Person who you are looking for is living at this address:");
+		System.out.println(obj_1.getName());
+		System.out.println(obj_1.getAdress());
+		System.out.println(obj_1.getPobox());
+
+
+		
+		
+	}
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+	private String name;
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public int getPobox() {
+		return pobox;
+	}
+	public void setPobox(int pobox) {
+		this.pobox = pobox;
+	}
+	private String adress;
+    private int pobox;
+	public void Adress(String adress2) {
+		
+	}
+	
+}
